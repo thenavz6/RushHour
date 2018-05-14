1b647eb25d4d56ae5db63aac81a449d21e4066c0
David <ddchen30@gmail.com>
2018-05-14 19:38:03 +1000

    frontend

---
+++ src/MainPiece.java
@@ -0,0 +1,38 @@
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+
+public class MainPiece extends Rectangle {
+
+    private int xPos;
+    private int yPos;
+
+    public MainPiece(int x, int y) {
+        this.xPos = x;
+        this.yPos = y;
+        System.out.println("x cooridnate =" + x + " " + "Y coordinate = " + y);
+        setWidth(RushHourApp.TILE_SIZE * 2);
+        setHeight(RushHourApp.TILE_SIZE);
+
+        // places it in the determined position
+        // if not it will default to the top
+
+        System.out.println("hello");
+        relocate(x * RushHourApp.TILE_SIZE, y * RushHourApp.TILE_SIZE);
+        setFill(Color.valueOf("#ff0000"));
+    }
+    public void moveRight(){
+        int newX = (this.xPos) + 100;
+        System.out.println("x cooridnate =" + this.xPos + " " + "Y coordinate = " + this.yPos);
+        this.xPos = newX;
+        //System.out.println(newX);
+        relocate(newX, this.yPos);
+
+    }
+    public void moveLeft(){
+        int newX = (this.xPos) - 100;
+        System.out.println("x cooridnate =" + this.xPos + " " + "Y coordinate = " + this.yPos);
+        this.xPos = newX;
+        //System.out.println(newX);
+        relocate(newX, this.yPos);
+    }
+}
